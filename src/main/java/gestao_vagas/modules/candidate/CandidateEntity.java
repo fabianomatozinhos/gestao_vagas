d81c9ea619e57f83456ed35d845ec15689fe0706
Fabiano Matozinhos <fabianomatozinhos1@hotmail.com>
2026-04-22 23:56:55 -0300

    Inserindo validações no request

--- src/main/java/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,6 +1,9 @@
 package gestao_vagas.modules.candidate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
@@ -9,8 +12,14 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
+
+    @Pattern(regexp = "\\S+", message = "O campo de username não deve conter espaço")
     private String username;
+
+    @Email(message = "O campo deve conter um e-mail válido")
     private String email;
+
+    @Length(min = 6, max = 100, message = "A senha deve conter entre 6 a 100 caracteres")
     private String password;
     private String description;
     private String curriculum;
